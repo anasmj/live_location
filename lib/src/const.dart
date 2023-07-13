582cc786c2007d9141117f45be99c62fd6967d2a
anas <anasmj7@gmail.com>
2023-07-13 15:33:23 +0600

    init commit

---
+++ lib/src/const.dart
@@ -0,0 +1,8 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+const googleApiKey = 'AIzaSyCDe-ZXbrr7bKx9BcMMfNt8iAZj3v0v-Fw';
+
+const lat = 23.780874057967676;
+
+const long = 90.40725890547037;
+const defaultLocation = LatLng(lat, long);
